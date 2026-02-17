30149e8e58f634b7501dfee65e22bfd76074e80c
Manoj Garje <manojgarje123@gmail.com>
2026-02-17 17:33:46 +0530

    Create a skeleton for auth services

---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const register = async (data) => {
+ 
+};
+
+export const login = async (data) => {
+
+};
+
+export const refresh = async (refreshToken) => {
+ 
+};
