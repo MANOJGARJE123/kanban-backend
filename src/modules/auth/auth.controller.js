1a9d96e5fe6fbd9fc483323070c32fcce5661d04
Manoj Garje <manojgarje123@gmail.com>
2026-02-17 17:30:25 +0530

    Change code to ES6

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,23 +1,22 @@
-import authService from './auth.service';
+import * as authService from './auth.service.js';
 
 export const register = async (req, res, next) => {
-    try{
-        const data = await authService.register(req.body);
-        res.status(201).json(data);
-
-    }catch(error){
-        next(error);
-    }
-}
+  try {
+    const data = await authService.register(req.body);
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const login = async (req, res, next) => {
-    try{
-        const data = await authService.login(req.body);
-        res.status(200).json(data);
-    }catch(error){
-        next(error)
-    }   
-}
+  try {
+    const data = await authService.login(req.body);
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const refresh = async (req, res, next) => {
   try {
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,9 +1,10 @@
-const express = require('express');
+import express from 'express';
+import * as authController from './auth.controller.js';
+
 const router = express.Router();
-const authController = require('./auth.controller');
 
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/refresh', authController.refresh);
 
-router.post("/regsiter", authController.register);
-router.post("/login", authController.login);
-
-module.exports = router;
+export default router;
